5e3d54abeb8b9f79c21957f4c3816c36b61b57bf
jonhealy1 <jonathan.d.healy@gmail.com>
2021-10-16 07:48:28 -0700

    simple validation

--- stac_check/main.py
+++ stac_check/main.py
@@ -1,8 +1,19 @@
 import click
+import json
+import jsonschema
+from jsonschema import validate
 
 def parse_file(file):
     f = open(file, "r")
-    print(f.read())
+    valid = validate_file(f)
+    print(valid)
+
+def validate_file(file):
+    try:
+        json.load(file)
+    except ValueError as err:
+        return False
+    return True
 
 @click.command()
 @click.argument('file')
