00eaf4338fe6eb30f86768cb47c39336a3949c2d
jonhealy1 <jonathan.d.healy@gmail.com>
2021-10-17 05:08:56 -0700

    change output to click secho

--- stac_check/cli.py
+++ stac_check/cli.py
@@ -6,14 +6,17 @@ def cli_message(file):
     linter = Linter(file)
     info = linter.parse_file()
     if info["stac_validator"]["version"] == "1.0.0":
-        click.echo(click.style(info["update"], fg='green'))
+        click.secho(info["update"], fg='green')
     else:
         click.secho(info["update"], fg='red')
-    click.echo(click.style("Validator: stac-validator 2.3.0 ", fg="blue"))
+    click.secho("Validator: stac-validator 2.3.0 ", fg="blue")
     if info["stac_validator"]["valid_stac"] == True:
-        click.echo(click.style(f"Valid stac: {info['stac_validator']['valid_stac']}", fg='green'))
+        click.secho(f"Valid stac: {info['stac_validator']['valid_stac']}", fg='green')
     else:
-        click.echo(click.style(f"Valid stac: {info['stac_validator']['valid_stac']}", fg='red'))
+        click.secho(f"Valid stac: {info['stac_validator']['valid_stac']}", fg='red')
+
+
+
     click.secho(json.dumps(info["stac_validator"], indent=4))
 
 @click.command()
