1a0c01b12ace2ded36200a4eaf4545e7c854f444
jonhealy1 <jonathan.d.healy@gmail.com>
2021-10-18 08:57:09 -0700

    reorg 1

--- stac_check/lint/lint.py
+++ stac_check/lint/lint.py
@@ -6,50 +6,55 @@ class Linter:
     item: str
 
     def __post_init__(self):
-        self.version = ""
-        self.message = {}
+        self.message = self.validate_file(self.item)
+        self.version = self.check_version()
+        self.link_format = self.check_link_format()
+        self.asset_type = self.check_asset_type()
         self.validator_version = "2.3.0"
         self.schema = []
-        self.update_msg = ""
-        self.asset_type = ""
+        self.update_msg = self.set_update_message()
         self.valid_stac = False
-        self.link_format = []
         self.error_type = ""
         self.error_msg = ""
 
+    def validate_file(self, file):
+        stac = stac_validator.StacValidate(file, links=True)
+        stac.run()
+        return stac.message[0]
+
     def parse_file(self):
-        self.message = self.validate_file(self.item)
-        self.check_version()
-        self.check_link_format()
         self.check_errors()
-        self.check_asset_type()
         self.schema = self.message["schema"]
         self.valid_stac = self.message["valid_stac"]
 
     def check_asset_type(self):
         if "asset_type" in self.message:
-            self.asset_type = self.message["asset_type"]
+            return self.message["asset_type"]
+        else:
+            return ""
 
     def check_version(self):
-        self.version = self.message["version"]
-        if self.version != "1.0.0":
-            self.update_msg = f"Please upgrade from version {self.version} to version 1.0.0!"
+        if "version" in self.message:
+            return self.message["version"]
         else:
-            self.update_msg = "Thanks for using STAC version 1.0.0!"
+            return ""
+
+    def set_update_message(self):
+        if self.version != "1.0.0":
+            return f"Please upgrade from version {self.version} to version 1.0.0!"
+        else:
+            return "Thanks for using STAC version 1.0.0!"
 
     def check_link_format(self):
         if "links_validated" in self.message:
-            self.link_format = self.message["links_validated"]["format_invalid"]
+            return self.message["links_validated"]["format_invalid"]
+        else:
+            return []
 
     def check_errors(self):
         if "error_type" in self.message:
             self.error_type = self.message["error_type"]
         if "error_message" in self.message:
             self.error_msg = self.message["error_message"]
-
-    def validate_file(self, file):
-        stac = stac_validator.StacValidate(file, links=True)
-        stac.run()
-        return stac.message[0]
 
     
