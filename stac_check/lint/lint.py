b7e2b8b9b2fcb36ab27c79c24f28bf9cf95107d7
jonhealy1 <jonathan.d.healy@gmail.com>
2021-10-18 03:13:06 -0700

    add bad item, checks

--- stac_check/lint/lint.py
+++ stac_check/lint/lint.py
@@ -20,7 +20,8 @@ class Linter:
         self.check_version()
         self.check_link_format()
         self.schema = self.message["schema"]
-        self.asset_type = self.message["asset_type"]
+        if "asset_type" in self.message:
+            self.asset_type = self.message["asset_type"]
         self.valid_stac = self.message["valid_stac"]
  
     def check_version(self):
@@ -31,7 +32,8 @@ class Linter:
             self.update_msg = "Thanks for using STAC version 1.0.0!"
 
     def check_link_format(self):
-        self.link_format = self.message["links_validated"]["format_invalid"]
+        if "links_validated" in self.message:
+            self.link_format = self.message["links_validated"]["format_invalid"]
 
     def validate_file(self, file):
         stac = stac_validator.StacValidate(file, links=True)
