b4ffd14158d84d3d72bdc04bb6e7fc326e18d697
jonhealy1 <jonathan.d.healy@gmail.com>
2021-11-06 01:17:53 -0700

    add asset format to linter class

--- stac_check/lint/lint.py
+++ stac_check/lint/lint.py
@@ -6,17 +6,18 @@ class Linter:
     item: str
 
     def __post_init__(self):
-        self.message = self.validate_file(self.item)
-        self.version = self.check_version()
-        self.invalid_link_format = self.check_link_format(10)
-        self.invalid_link_request = self.check_link_request(10)
         self.asset_type = self.check_asset_type()
+        self.version = self.check_version()
         self.validator_version = "2.3.0"
-        self.schema = self.check_schema()
         self.update_msg = self.set_update_message()
         self.valid_stac = self.message["valid_stac"]
         self.error_type = self.check_error_type()
         self.error_msg = self.check_error_message()
+        self.invalid_asset_format = self.check_asset_format(10)
+        self.invalid_link_format = self.check_link_format(10)
+        self.invalid_link_request = self.check_link_request(10)
+        self.message = self.validate_file(self.item)
+        self.schema = self.check_schema()
 
     def validate_file(self, file):
         stac = stac_validator.StacValidate(file, links=True)
@@ -46,6 +47,16 @@ class Linter:
             return f"Please upgrade from version {self.version} to version 1.0.0!"
         else:
             return "Thanks for using STAC version 1.0.0!"
+
+    def check_asset_format(self, num_links):
+        invalid_assets_format = []
+        if "assets_validated" in self.message:
+            for invalid_format_url in self.message["assets_validated"]["format_invalid"]:
+                invalid_assets_format.append(invalid_format_url)
+                num_links = num_links - 1
+                if num_links == 0:
+                    return invalid_assets_format
+        return invalid_assets_format
 
     def check_link_format(self, num_links):
         invalid_links_format = []
