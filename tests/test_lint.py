d425d90c226a6ac9ac4d04c24b8e512a2da40292
jonhealy1 <jonathan.d.healy@gmail.com>
2021-11-06 00:45:24 -0700

    check link format errors

--- tests/test_lint.py
+++ tests/test_lint.py
@@ -1,8 +1,11 @@
 from stac_check.lint.lint import Linter
 
-def test_linter():
+def test_linter_bad_links():
     file = "sample_files/1.0.0/core-item-bad-links.json"
     linter = Linter(file)
+    link_format_errors = ["http:/remotdata.io/catalog/20201211_223832_CS2/index.html"]
     assert linter.version == "1.0.0"
     assert linter.valid_stac == True
-    assert linter.asset_type == "Item"
+    assert linter.asset_type == "ITEM"
+    assert len(linter.invalid_link_format) > 0
+    assert linter.invalid_link_format == link_format_errors
