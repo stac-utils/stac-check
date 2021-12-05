d03f0ff6fa526757ab17869b50676d366eb1c009
jonhealy1 <jonathan.d.healy@gmail.com>
2021-12-05 06:53:34 -0800

    revert setup

--- setup.py
+++ setup.py
@@ -2,14 +2,9 @@
 """
 from setuptools import setup, find_packages
 
-__version__ = "2.2.0"
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setup(
     name="stac_check",
-    version=__version__,
+    version="0.1.0",
     description="Linting and validation tool for STAC assets",
     url="https://github.com/jonhealy1/stac-check",
     packages=find_packages(),
@@ -17,11 +12,12 @@ setup(
         "click",
         "pystac[validation]==1.1.0"
     ],
+    dependency_links=[
+        "https://test.pypi.org/simple/"
+    ],
     entry_points={
         'console_scripts': ['stac_check=stac_check.cli:main']
     },
     author="Jonathan Healy",
-    license="MIT",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
+    license="MIT"
 )
