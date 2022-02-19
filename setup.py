9dda9b54dae4b0e3051e3b830ee36a6487ecb941
jonhealy1 <jonathan.d.healy@gmail.com>
2022-02-19 07:09:02 -0800

    version 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 """
 from setuptools import setup, find_packages
 
-__version__ = "0.1.3"
+__version__ = "0.2.0"
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -19,7 +19,8 @@ setup(
         "click>=7.1.2",
         "requests>=2.19.1",
         "jsonschema>=3.1.2b0",
-        "pytest"
+        "pytest",
+        "stac-validator==2.3.0"
     ],
     entry_points={
         'console_scripts': ['stac_check=stac_check.cli:main']
