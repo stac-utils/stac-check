be2e672a38356a398892443f519e8bac9bee9618
jonhealy1 <jonathan.d.healy@gmail.com>
2022-04-28 02:57:37 -0700

    rearrange recursion

--- stac_check/cli.py
+++ stac_check/cli.py
@@ -28,74 +28,78 @@ def cli_message(linter):
 
     click.secho()
 
-    ''' validator used - pystac if recursive otherwise stac-validator '''
-    if linter.recursive == True:
-        click.secho(f"Validator: pystac 1.1.0", bg="blue", fg="white")
+    click.secho(f"Validator: stac-validator {linter.validator_version}", bg="blue", fg="white")
+
+    if linter.recursive > 0:
         click.secho(f"    Recursive: Validate all assets in a collection or catalog")
+        click.secho(f"    Assets Validated: ")
+        click.secho()
+        for msg in linter.validate_all:
+            click.secho(f"Version {msg['version']}")
+            click.secho(f"Path {msg['path']}")
+            click.secho(f"Schemas {msg['schema']}")
+            click.secho(f"Type {msg['asset_type']}")
+            click.secho(f"Valid {msg['valid_stac']}")
+            click.secho()
+
     else:
-        click.secho(f"Validator: stac-validator {linter.validator_version}", bg="blue", fg="white")
-
-    click.secho()
-    
-    ''' valid stac object message - true or false '''
-    if linter.valid_stac == True:
-        click.secho(f"Valid {linter.asset_type}: {linter.valid_stac}", fg='green')
-    else:
-        click.secho(f"Valid {linter.asset_type}: {linter.valid_stac}", fg='red')
-
-    ''' schemas validated for core object '''
-    click.secho()
-    if len(linter.schema) > 0:
-        click.secho("Schemas validated: ", fg="blue")
-        for schema in linter.schema:
-            click.secho(f"    {schema}")
-
-    ''' best practices message'''
-    click.secho()
-    for message in linter.best_practices_msg:
-        if message == linter.best_practices_msg[0]:
-            click.secho(message, bg='blue')
+        click.secho()
+        
+        ''' valid stac object message - true or false '''
+        if linter.valid_stac == True:
+            click.secho(f"Valid {linter.asset_type}: {linter.valid_stac}", fg='green')
         else:
-            click.secho(message, fg='red')
+            click.secho(f"Valid {linter.asset_type}: {linter.valid_stac}", fg='red')
 
-    if linter.validate_all == True:
+        ''' schemas validated for core object '''
         click.secho()
-        click.secho(f"Recursive validation has passed!", fg='blue')
-    elif linter.validate_all == False and linter.recursive == True:
+        if len(linter.schema) > 0:
+            click.secho("Schemas validated: ", fg="blue")
+            for schema in linter.schema:
+                click.secho(f"    {schema}")
+
+        ''' best practices message'''
         click.secho()
-        click.secho(f"Recursive validation has failed!", fg='red')
+        for message in linter.best_practices_msg:
+            if message == linter.best_practices_msg[0]:
+                click.secho(message, bg='blue')
+            else:
+                click.secho(message, fg='red')
 
-    if linter.invalid_asset_format is not None:
+        if linter.validate_all == True:
+            click.secho()
+            click.secho(f"Recursive validation has passed!", fg='blue')
+        elif linter.validate_all == False and linter.recursive > 0:
+            click.secho()
+            click.secho(f"Recursive validation has failed!", fg='red')
+
+        if linter.invalid_asset_format is not None:
+            click.secho()
+            link_asset_message(linter.invalid_asset_format, "asset", "format")
+
+        if linter.invalid_asset_request is not None:
+            click.secho()
+            link_asset_message(linter.invalid_asset_request, "asset", "request")
+
+        if linter.invalid_link_format is not None:
+            click.secho()
+            link_asset_message(linter.invalid_link_format, "link", "format")
+
+        if linter.invalid_link_request is not None:
+            click.secho()
+            link_asset_message(linter.invalid_link_request, "link", "request")
+
+        if linter.error_type != "":
+            click.secho(f"Validation error type: ", fg="red")
+            click.secho(f"    {linter.error_type}")
+
+        if linter.error_msg != "":
+            click.secho(f"Validation error message: ", fg='red')
+            click.secho(f"    {linter.error_msg}")
+
         click.secho()
-        link_asset_message(linter.invalid_asset_format, "asset", "format")
 
-    if linter.invalid_asset_request is not None:
-        click.secho()
-        link_asset_message(linter.invalid_asset_request, "asset", "request")
-
-    if linter.invalid_link_format is not None:
-        click.secho()
-        link_asset_message(linter.invalid_link_format, "link", "format")
-
-    if linter.invalid_link_request is not None:
-        click.secho()
-        link_asset_message(linter.invalid_link_request, "link", "request")
-
-    if linter.error_type != "":
-        click.secho(f"Validation error type: ", fg="red")
-        click.secho(f"    {linter.error_type}")
-
-    if linter.error_msg != "":
-        click.secho(f"Validation error message: ", fg='red')
-        click.secho(f"    {linter.error_msg}")
-
-    if linter.recursive_error_msg != "":
-        click.secho(f"Recursive validation error message: ", fg='red')
-        click.secho(f"    {linter.recursive_error_msg}")
-
-    click.secho()
-
-    click.secho(f"This object has {len(linter.data['links'])} links")
+        click.secho(f"This object has {len(linter.data['links'])} links")
 
     click.secho()
 
@@ -103,7 +107,11 @@ def cli_message(linter):
     # click.secho(json.dumps(linter.message, indent=4))
 
 @click.option(
-    "-r", "--recursive", is_flag=True, help="Validate all assets in a collection or catalog."
+    "--recursive",
+    "-r",
+    type=int,
+    default=0,
+    help="Maximum depth to traverse when using recursion.",
 )
 @click.option(
     "-a", "--assets", is_flag=True, help="Validate assets for format and response."
@@ -114,6 +122,6 @@ def cli_message(linter):
 @click.command()
 @click.argument('file')
 @click.version_option(version="1.2.0")
-def main(file, assets, links, recursive):
-    linter = Linter(file, assets, links, recursive)
+def main(file, recursive, assets, links):
+    linter = Linter(file, assets=assets, links=links, recursive=recursive)
     cli_message(linter)
